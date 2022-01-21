2b16ada9ccc36635972de3a6062af98bcefb4762
Jazmin-Ewers <jazminewers11@gmail.com>
2022-01-21 18:02:24 -0500

    Complete Step 3.2

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# The Widget Model only has two fields:
+# description - a CharField with a max_length of your choosing
+# quantity - an IntegerField
+
+class Widget(models.Model):
+    desciption = models.CharField(max_length=200)
+    quantity = models.IntegerField()
+
+
+
